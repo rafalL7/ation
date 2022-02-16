a7e87442836a93ec9952580077e17efde06fabb3
rafalL7 <rafal.sarnecki@gum.gov.pl>
2022-02-16 15:03:55 +0100

    implement  of User class i Action class

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -1,9 +1,9 @@
 public class Action {
     public static void main(String[] args) {
+User name= new User();
 
-
-        String name="Rafał";
-        System.out.println(name);
+      name.setName("Rafał");
+        System.out.println(name.getName());
 
     }
 }
